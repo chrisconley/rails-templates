7b03166928fff23081654f7bf05da99b53cbdc28
Chris Conley <chris.m.conley@gmail.com>
2009-06-18 17:24:14 -0400

    modified base template

--- base.rb
+++ base.rb
@@ -2,26 +2,31 @@ git :init
 # Delete unnecessary files
   run "echo TODO > README"
   run "rm public/index.html"
-  run "rm public/favicon.ico"
-  run "rm public/robots.txt"
   run "rm -f public/javascripts/*"
   
 # Download JQuery
-  run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.1.min.js > public/javascripts/jquery.js"
+  run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js"
   run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
   
 # Download Blueprint CSS
-  git :clone => "git://github.com/joshuaclayton/blueprint-css.git public/stylesheets/blueprint"
- 
-plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication.git', :submodule => true
-plugin 'role_requirement', :git => 'git://github.com/timcharper/role_requirement.git', :submodule => true
-plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git', :submodule => true
+  git :clone => "git://github.com/joshuaclayton/blueprint-css.git blueprint"
+  run 'mv blueprint/blueprint/*.css public/stylesheets'
+  run 'rm -rf blueprint/'
+  
+  
+# Plugins
+  plugin 'paperclip', :git => "git://github.com/thoughtbot/paperclip.git"
+  plugin 'exceptional', :git => 'git://github.com/contrast/exceptional.git'
+  run 'cat vendor/plugins/exceptional/exceptional.yml | sed "s/PASTE_YOUR_API_KEY_HERE/f088abbf3c2bad0180f45780fd3358f4b747dcc7/" > config/exceptional.yml'
+
+gem "rspec", :lib => false, :version => ">=1.2.6"
+gem "rspec-rails", :lib => false, :version => ">=1.2.6"
 gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
-gem 'ruby-openid', :lib => 'openid'
-gem 'sqlite3-ruby', :lib => 'sqlite3'
+gem "webrat"
+gem "cucumber"
 gem 'haml'
-gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-#gem 'nifty-generators'
+gem "right_aws"
+
 
 sentinel = 'Rails::Initializer.run do |config|'
 session_code = "config.action_controller.session = { :key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }"
@@ -32,29 +37,75 @@ in_root do
 end
 
 # Set up session store initializer
-  initializer 'session_store.rb', <<-END
-ActionController::Base.session = { :session_key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }
-ActionController::Base.session_store = :active_record_store
-  END
+#   initializer 'session_store.rb', <<-END
+# ActionController::Base.session = { :session_key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }
+# ActionController::Base.session_store = :active_record_store
+#   END
 
 rake('gems:install', :sudo => true)
 
 in_root do
   run "haml --rails ."
 end
-generate("nifty_layout application --haml")
-rake('db:sessions:create')
-generate("authenticated", "user session --rspec")
-generate("roles", "Role User")
-rake('open_id_authentication:db:create')
+
 rake('db:migrate')
 
-
+route "map.root :controller => 'users', :action => 'new'"
  
-if yes?("Do you want to use RSpec?")
-  run "rm -rf test"
-  generate :rspec
-end
+run "rm -rf test"
+generate :rspec
+
+# get app name and insert in this file
+file "config/app_config.yml", <<-END
+development:
+  domain: localhost:3000
+  storage: s3
+  bucket: wdi-dev
+  path: ':attachment/:id/:style/:basename.:extension'
+  access_key_id: 06F1Q32S0MFHXQJ9ER02
+  secret_access_key: UylNnoWLGTxArO9sxQlitEcELKbyvDSqLz3iOqMY
+  image_magick_path: '/opt/local/bin'
+staging:
+  domain: staging.dealblinker.com
+  storage: s3
+  bucket: wdi-staging
+  path: ':attachment/:id/:style/:basename.:extension'
+  access_key_id: 06F1Q32S0MFHXQJ9ER02
+  secret_access_key: UylNnoWLGTxArO9sxQlitEcELKbyvDSqLz3iOqMY
+production:
+  domain: www.dealblinker.com
+  storage: s3
+  bucket: wdi-production
+  path: ':attachment/:id/:style/:basename.:extension'
+  access_key_id: 06F1Q32S0MFHXQJ9ER02
+  secret_access_key: UylNnoWLGTxArO9sxQlitEcELKbyvDSqLz3iOqMY
+test:
+  domain: something.com
+  storage: filesystem
+  access_key_id: 06F1Q32S0MFHXQJ9ER02
+END
+
+file "config/initializers/_load_app_config.rb", <<-'END'
+APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/app_config.yml")[RAILS_ENV].symbolize_keys
+Paperclip.options[:command_path] = APP_CONFIG[:image_magick_path]
+END
+
+file "app/views/layouts/application.html.haml", <<-'END'
+!!! Strict
+%html{html_attrs}
+  
+  %head
+    %title
+      = yield(:page_title) ? yield(:page_title) + " | " : "" 
+      = "WhoDesigned.it"
+    %meta{"http-equiv"=>"Content-Type", :content=>"text/html; charset=utf-8"}
+    = javascript_include_tag 'jquery', 'jquery.form', :cache => true
+    = stylesheet_link_tag 'screen', 'application', :cache => true
+    = yield(:head)
+  
+  %body
+    .container
+END
  
 file ".gitignore", <<-END
 .DS_Store
@@ -66,7 +117,6 @@ END
  
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
 run "cp config/database.yml config/example_database.yml"
-
-
  
-git :add => ".", :commit => "-m 'initial commit'"
+git :add => "."
+git :commit => "-m 'initial commit'"
